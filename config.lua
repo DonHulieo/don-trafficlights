028db14c19d374518a5aeff97d81e2b0d643118f
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-08-09 20:42:36 +1000

    Refactor and Optimisation
    Changed the main thread to only be triggered when a player enters a vehicle, alleviating dormant resource usage and huge spikes whilst in use.
    Made use of Scoped Events to eliminate Network Event spam, now only players within 211u of the driver will receive changed lights.

--- config.lua
+++ config.lua
@@ -1,82 +1,53 @@
 Config = {}
 
-Config.Framework = 'ESX' -- 'ESX' or 'QBCore'
+Config.Framework = 'QBCore' -- 'ESX' or 'QBCore' or 'None' (if you want to use this script without any job checks and just use the siren to trigger the lights)
 
 Config.TrafficLights = {
     search = {
-        step = 10.0, 
-        minDist = 20.0,
-        maxDist = 60.0,
-        rad = 10.0,
-        headingThres = 40.0
+        maxDist = 60.0, -- Max distance to search for traffic lights
+        headingThres = 40.0, -- The threshold for the heading difference between player and the traffic light
+        maxLights = 4, -- Max amount of lights to set at once
     },
     duration = {
-        civCheckTime = 3000,
-        leoCheckTime = 1000,
-        lightTime = 3500,
-        leoLightTime = 7500 
-    }
+        civCheckTime = 500, -- Time in ms to check for traffic lights as a civilian
+        leoCheckTime = 500, -- Time in ms to check for traffic lights as a leo
+        lightTime = 3500, -- Time in ms until the light changes
+        leoLightTime = 7500,  -- Time in ms until the light changes for leo light changes
+    },
 }
 
 Config.JobCheck = 'FirstResponder' -- Set to either LEO, EMS or FirstResponder to change what jobs declared below, trigger a light change
 
 
-if Config.Framework == 'QBCore' then 
-    local QBCore = exports['qb-core']:GetCoreObject()
-    Config.AuthorizedJobs = 
-    {
-        LEO = { -- this is for job checks which should only return true for police officers
-            Jobs = {['police'] = true, ['fib'] = true, ['sheriff'] = true},
-            Types = {['police'] = true, ['leo'] = true},
-            Check = function()
-                local PlyData = QBCore.Functions.GetPlayerData()
-                local job, jobtype = PlyData.job.name, PlyData.job.type
-                if Config.AuthorizedJobs.LEO.Jobs[job] or Config.AuthorizedJobs.LEO.Types[jobtype] then return true end
-            end
-        },
-        EMS = { -- this if for job checks which should only return true for ems workers
-            Jobs = {['ambulance'] = true, ['fire'] = true},
-            Types = {['ambulance'] = true, ['fire'] = true, ['ems'] = true},
-            Check = function()
-                local PlyData = QBCore.Functions.GetPlayerData()
-                local job, jobtype = PlyData.job.name, PlyData.job.type
-                if Config.AuthorizedJobs.EMS.Jobs[job] or Config.AuthorizedJobs.EMS.Types[jobtype] then return true end
-            end
-        },
-        FirstResponder = { -- do not touch, this is a combined job checking function for emergency services (police and ems)
-            Check = function()
-                local PlyData = QBCore.Functions.GetPlayerData()
-                local job, jobtype = PlyData.job.name, PlyData.job.type
-                if Config.AuthorizedJobs.LEO.Check(jobtype, job) or Config.AuthorizedJobs.EMS.Check(jobtype, job) then return true end            
-            end
-        }
+--------------------------- ADD YOUR RELEVANT EMS JOBS HERE // DO NOT TOUCH THE FIRST RESPONDER SECTION ---------------------------
+Config.AuthorizedJobs = {
+    LEO = { -- this is for job checks which should only return true for police officers
+        Jobs = {['police'] = true, ['fib'] = true, ['sheriff'] = true},
+        Types = {['police'] = true, ['leo'] = true},
+        Check = function(PlyData)
+            PlyData = PlyData or GetPlayerData()
+            if not PlyData or (PlyData and (not PlyData.job or not PlyData.job.type))  then return false end
+            local job, jobtype = PlyData.job.name, PlyData.job.type
+            if Config.AuthorizedJobs.LEO.Jobs[job] or Config.AuthorizedJobs.LEO.Types[jobtype] then return true end
+        end
+    },
+    EMS = { -- this if for job checks which should only return true for ems workers
+        Jobs = {['ambulance'] = true, ['fire'] = true},
+        Types = {['ambulance'] = true, ['fire'] = true, ['ems'] = true},
+        Check = function(PlyData)
+            PlyData = PlyData or GetPlayerData()
+            if not PlyData or (PlyData and (not PlyData.job or not PlyData.job.type))  then return false end
+            local job, jobtype = PlyData.job.name, PlyData.job.type
+            if Config.AuthorizedJobs.EMS.Jobs[job] or Config.AuthorizedJobs.EMS.Types[jobtype] then return true end
+        end
+    },
+    FirstResponder = { -- do not touch, this is a combined job checking function for emergency services (police and ems)
+        Check = function(PlyData)
+            PlyData = PlyData or GetPlayerData()
+            if not PlyData or (PlyData and (not PlyData.job or not PlyData.job.type))  then return false end
+            local job, jobtype = PlyData.job.name, PlyData.job.type
+            if Config.AuthorizedJobs.LEO.Check(PlyData, jobtype, job) or Config.AuthorizedJobs.EMS.Check(PlyData, jobtype, job) then return true end            
+        end
     }
-elseif Config.Framework == 'ESX' then
-    local ESX = exports['es_extended']:getSharedObject()
-    Config.AuthorizedJobs = 
-    {
-        LEO = { -- this is for job checks which should only return true for police officers
-            Jobs = {['police'] = true, ['fib'] = true, ['sheriff'] = true, ['highway'] = true},
-            Check = function()
-                local PlyData = ESX.GetPlayerData()
-                local job = PlyData.job.name
-                if Config.AuthorizedJobs.LEO.Jobs[job] then return true end
-            end
-        },
-        EMS = { -- this if for job checks which should only return true for ems workers
-            Jobs = {['ambulance'] = true, ['fire'] = true},
-            Check = function()
-                local PlyData = ESX.GetPlayerData()
-                local job = PlyData.job.name
-                if Config.AuthorizedJobs.EMS.Jobs[job] then return true end
-            end
-        },
-        FirstResponder = { -- do not touch, this is a combined job checking function for emergency services (police and ems)
-            Check = function()
-                local PlyData = ESX.GetPlayerData()
-                local job = PlyData.job.name
-                if Config.AuthorizedJobs.LEO.Check(job) or Config.AuthorizedJobs.EMS.Check(job) then return true end            
-            end
-        }
-    }
-end
+}
+------------------------------------------------------------------------------------------------------------------------------
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DonHulieo'
+description 'Traffic Lights Logic for Fivem'
+version '1.0.9'
+
+shared_scripts {'shared/config.lua', 'shared/lib.lua'}
+
+client_script 'client/main.lua'
+
+server_script 'server/main.lua'
+
+escrow_ignore 'config.lua'
+
+lua54 'yes'
